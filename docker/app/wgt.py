67595e5cf3045db7700f65bad9f81d53411bf1fe
Terence Davis <lisnter@verizon.net>
2019-11-10 18:55:30 -0800

    Changed to pick up vault URL from environment.  A little code movement/comments.

--- docker/app/wgt.py
+++ docker/app/wgt.py
@@ -3,8 +3,6 @@
 #
 #  Presents a single URL to retrieve the credentials associated with the provided key
 #
-#  SBD_JWT_KEY='abcd'; SBD_JWT_SECRET='123'; export SBD_JWT_KEY; export SBD_JWT_SECRET
-#
 from azure.keyvault import KeyVaultClient
 from azure.common.credentials import ServicePrincipalCredentials
 import os
@@ -15,26 +13,24 @@ import jwt
 #
 ID_WGT_PORT = 27182  #  port is e
 ID_WGT_JWT_ENCODE = 'HS256'
-ID_WGT_ENV_JWT_KEY = 'SBD_JWT_KEY'  #  environment variable with encryption key for the JWT header (from kubernetes)
-ID_WGT_ENV_JWT_SECRET = 'SBD_JWT_SECRET'  #  Environment variable with shared secret value (injected by kubernetes)
-ID_WGT_JWT_PAYLOAD_JSON_KEY = 'JWTToken'  #  JSON key name in payload that has the provided shared secret (must match SBD_JWT_SECRET)
+
+#  Environment vars configurable per-deployment; these are set by Terraform per deployment so that
+#  code doesn't need to be changed.  Terraform injects these variables into the environment so they can be
+#  accessed by the container.  If any of them are missing then this subsystem should fail
+#
+ID_WGT_ENV_JWT_KEY = 'SBD_JWT_KEY'  #  encryption key for the JWT header
+ID_WGT_ENV_JWT_SECRET = 'SBD_JWT_SECRET'  #  agreed shared secret value
+ID_WGT_JWT_PAYLOAD_JSON_KEY = 'JWTToken'  #  JSON key name in payload that has the provided shared secret from the caller (must match SBD_JWT_SECRET)
+ID_WGT_AZURE_CLIENT_ID = 'TF_VAR_client_id'  #  Azure vault credentials
+ID_WGT_AZURE_SECRET    = 'TF_VAR_client_secret'  #  ditto
+ID_WGT_AZURE_TENANT    = 'TF_VAR_tenantId'  #  ditto
+ID_WGT_AZURE_VAULT_URL = 'SBD_VAULT_URL'  #  Azure vault URL
 
 #  Errors and return values
 ID_ERR_MISSING_SID = '__noSecret__'  #  no secret ID was placed on the URL
 ID_STATUS_OK = 0
 ID_STATUS_MISSING_SECRET = 1000
 ID_STATUS_SECRET_NOT_FOUND = 1001
-
-
-#  hard-code this stuff for now; eventually will pick it up from kubernetes
-#
-#  REMOVE THESE TWO BEFORE CHECKIN
-sbdVaultCreds = ServicePrincipalCredentials(
-    client_id = '',
-    secret =    '',
-    tenant =    '' )
-
-sbdVaultURL = 'https://mics-kv.vault.azure.net/'
 
 
 #  Handle the JWT part since it's several steps.  Any failure along the way just raise
@@ -105,20 +101,35 @@ class SBDSecret( object ):
     def secret( self, secretID=ID_ERR_MISSING_SID, *args ):  #  secretID='__noSecret__' to catch if no secretID is provided
         rc = { 'status':ID_STATUS_MISSING_SECRET, 'secretID':secretID }  #  assume an error
         if( len( args ) == 0 and not secretID == ID_ERR_MISSING_SID ):  #  if properly formatted args will be empty and secretID will be something other than ID_SEC_MISSING_SID
-            vaultClient = KeyVaultClient( sbdVaultCreds )
-#            try:
-            sBundle = vaultClient.get_secret( sbdVaultURL, secretID, '' )  #  regarding the ''. . .should really use vC.get_secret_versions() but I fiddled with that all day and it seems broken (thanks Zach for the simple workaround!)
-            secretVal = sBundle.value
-            rc = { 'status':ID_STATUS_OK, 'secretID':secretID, 'secret':secretVal }
-#            except:  #  any exception just return error
-#                rc = { 'status':ID_STATUS_SECRET_NOT_FOUND, 'secretID':secretID }
+            vaultClient = KeyVaultClient( sbdVaultCreds )  #  reauthenticate each time so if anything changed we will fail (is this a good idea?)
+            try:
+                sBundle = vaultClient.get_secret( sbdVaultURL, secretID, '' )  #  regarding the ''. . .should really use vC.get_secret_versions() but I fiddled with that all day and it seems broken (thanks Zach for the simple workaround!)
+                secretVal = sBundle.value
+                rc = { 'status':ID_STATUS_OK, 'secretID':secretID, 'secret':secretVal }
+            except:  #  any exception just return error
+                rc = { 'status':ID_STATUS_SECRET_NOT_FOUND, 'secretID':secretID }
 
         return rc
 
 
 if __name__ == '__main__':
+#  If any of the steps in __main__ fail then just fail the startup.  This is OK since if this component doesn't start up
+#  then something is wrong and we want to fail all requests
+#
+#  Get the JWT items from the environment
     jwtKey = os.environ[ ID_WGT_ENV_JWT_KEY ]  #  They encryption key used to encrypt the JWT authorization header
     jwtSecret = os.environ[ ID_WGT_ENV_JWT_SECRET ]  #  the secret encoded in the JWT payload; will have been injected by kubernetes
+
+#  get the vault items from the environment
+    sbdVaultCreds = ServicePrincipalCredentials(
+        client_id = os.environ[ ID_WGT_AZURE_CLIENT_ID ],
+        secret =    os.environ[ ID_WGT_AZURE_SECRET ],
+        tenant =    os.environ[ ID_WGT_AZURE_TENANT ] )
+
+    sbdVaultURL = os.environ[ ID_WGT_AZURE_VAULT_URL ]
+
+#  Configure cherrypy so it listens properly (0.0.0.0 is in docs somewhere; Daren says it's OK if we configure the firewall correctly)
+#  and startup cherrypy
     cherrypy.config.update({'server.socket_host':'0.0.0.0', 'server.socket_port': ID_WGT_PORT})
     cherrypy.quickstart( SBDSecret() )
 
